20f7b78f93749295605ac9a147da26c9eff43d2d
Joaquin Tobal <JTOBAL@lomanegra.com>
2024-07-24 22:53:06 -0300

    Se cambia el uso de FS por el uso de Fetch

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var bbdd = fs.readFileSync('./bbdd.txt', "utf8");
-console.log(bbdd);
-var datosProductos = fs.readFileSync('./bbdd.txt', 'utf-8');
-var productosSinEspacios = datosProductos.trim();
-var productosArray = productosSinEspacios.split("|");
-console.log(productosArray);
-var productosJSON = [];
-for (var i = 0; i < productosArray.length; i++) {
-    var productosPorLinea = productosArray[i].split(",");
-    var nombre = productosPorLinea[0];
-    var precio = productosPorLinea[1];
-    var stock = productosPorLinea[2];
-    var producto = {
-        nombre: nombre,
-        precio: precio,
-        stock: stock
-    };
-    productosJSON.push(producto);
-}
-var pepito = JSON.stringify(productosJSON, null, 2);
-console.log(pepito);
+
+fetch('./bbdd.txt')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(datosProductos => {
+        console.log(datosProductos);
+
+        var productosSinEspacios = datosProductos.trim();
+        var productosArray = productosSinEspacios.split("|");
+        console.log(productosArray);
+
+        var productosJSON = [];
+        for (var i = 0; i < productosArray.length; i++) {
+            var productosPorLinea = productosArray[i].split(",");
+            var nombre = productosPorLinea[0];
+            var precio = productosPorLinea[1];
+            var stock = productosPorLinea[2];
+            var producto = {
+                nombre: nombre,
+                precio: precio,
+                stock: stock
+            };
+            productosJSON.push(producto);
+        }
+
+        var pepito = JSON.stringify(productosJSON, null, 2);
+        console.log(pepito);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
